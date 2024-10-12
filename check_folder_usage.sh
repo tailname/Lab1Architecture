3c68424f1400253fd72ef00bc2f4ac9f391fa0d6
Matvey <samiyto7@yandex.ru>
2024-10-12 16:33:07 +0300

    Add and SAVE some functions

--- check_folder_usage.sh
+++ check_folder_usage.sh
@@ -3,7 +3,7 @@
 
 # Проверяем, переданы ли аргументы
 
-if [ $# -ne 3 ]; then
+if [ $# -ne 2 ]; then
   echo "Неверное количество аргуметов !"
   exit 1
 fi
@@ -17,17 +17,25 @@ if [ ! -d "$folder_path" ]; then
   exit 1
 fi
 
-if [ ! ["$2" =~ ^[0-9]+] ]; then
+if [[ ! $2 =~ ^[0-9]+ ]]; then
   echo "Вы передали не число !"
   exit 1
 fi
 
-if [ ! ["$3" =~ ^[0-9]+] ]; then
-  echo "Вы передали не число !"
-  exit 1
-fi
-# Выводим результат
-echo "Заполнение папки '$folder_path': $(df $folder_path |  awk 'NR==2 {print $5}')"
+# Выводим процент заполненности
 
+persent_of_full=$(df $folder_path |  awk 'NR==2 {print $5}' | awk -F '%' '{print $1}')
+echo "Заполнение папки '$folder_path': $persent_of_full %"
+
+# Сравниваем с аргументами
+if [ $persent_of_full -lt $2 ]; then
+  exit 0
+fi
+N=10
+oldest_files=$(find $folder_path -type f -printf '%T+ %p\n' | sort | awk -F '$folder_path' '{print $2}')
+for file in $oldest_files; do
+  echo "Удаляем файл $file"
+done
+$(tar -czvf $HOME/backup/oldest_files.tar.gz -C $folder_path $oldest_files)
 # Возвращаем код 0, если все прошло успешно
 exit 0
